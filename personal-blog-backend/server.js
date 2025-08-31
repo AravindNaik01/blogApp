b7579d0471eee9e96acf58818510f620e58129d1
aravind <naikaravind044@gmail.com>
2025-09-01 00:28:37 +0530

    done

--- personal-blog-backend/server.js
+++ personal-blog-backend/server.js
@@ -11,7 +11,7 @@ const whitelist = [process.env.FRONTEND_URL];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (whitelist.indexOf(origin) !== -1 || !origin ) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
